f423c53148bd554b4a0d1b853dfb3a5beaa69edb
Kinght <1903847462@qq.com>
2017-03-19 15:35:52 +0800

    Add files via upload

---
+++ python basic.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 16:03:45 2017
+
+@author: ycb
+"""
+
+#输出hello，world
+print'hello,world'
+
+#输出多字符串，逗号为空格
+print 'The quick brown fox', 'jumps over', 'the lazy dog'
+
+#python科学计算
+
+#四则运算+/-/*//
+print 100+100
+
+#python做除法是精准的
+print 10/3
+print 10.0/3
+
+#幂方
+print 2**3
+
+#取余
+print 10%3
+
+#输入函数raw_input
+name=raw_input('Please inter you name:')
+print 'hello,',name
